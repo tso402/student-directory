acfa71eb9919791eff32a5ca647890edf57009f1
tso402 <james.ault@johnlewis.co.uk>
2019-02-01 17:51:44 +0000

    Created a student input method to create dynamic student list

--- directory.rb
+++ directory.rb
@@ -26,7 +26,26 @@ end
 def print_footer(students) # prints a statment on the number of students
 puts "Overall, we have #{students.count} great students"
 end
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    # create and empty array
+    students = []
+    # get the first name
+    name = gets.chomp
+    # while the name is not empty, repeat this code
+    while !name.empty? do
+        # add the student hash to the array
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        # get the next name from the user
+        name = gets.chomp
+    end
+    # return the array of students
+    students
+end
 #nothing happens if we dont call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
