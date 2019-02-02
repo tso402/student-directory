81214184b71b2f3f2e678f91504c52476972aa23
tso402 <james.ault@johnlewis.co.uk>
2019-02-02 12:55:47 +0000

    Corrected mistake.rb program

--- mistake.rb
+++ mistake.rb
@@ -2,7 +2,7 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
-  students = {}
+  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
@@ -18,22 +18,22 @@ def input_students
 end
 
 def print_header
-  put "The students of my cohort at Makers Academy'
-  puts ``-------------``
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
 
 def print(students)
-  student.each do /student/
-    puts '#{student[:name]} (#{student[:cohort]} cohort)'
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have {names.count} great students"
+  puts "Overall, we have #{names.count} great students"
 end
 
 
 students = input_students
 print_header
-print{students}
-print_footer[students]
+print(students)
+print_footer(students)
